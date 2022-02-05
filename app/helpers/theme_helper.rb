e9ca61af90efaf4fd849795b330e10a2b975aeb9
Kevin Diale <powersurge360@gmail.com>
2022-02-05 14:10:55 -0500

    Fixed full screen, corrected weird hover effect on mobile

--- app/helpers/theme_helper.rb
+++ app/helpers/theme_helper.rb
@@ -6,10 +6,10 @@ module ThemeHelper
   end
 
   def submit_button_class
-    "rounded-xl text-amber-300 mb-2 py-2 font-bold mt-auto hover:bg-purple-500 bg-purple-800"
+    "rounded-xl text-amber-300 mb-2 py-2 font-bold mt-auto lg:hover:bg-purple-500 focus:bg-purple-500 bg-purple-800"
   end
 
   def button_class
-    "font-bold text-amber-300 rounded-xl mb-2 py-2 bg-purple-800 hover:bg-purple-500"
+    "font-bold text-amber-300 rounded-xl mb-2 py-2 bg-purple-800 lg:hover:bg-purple-500 focus:bg-purple-500"
   end
 end

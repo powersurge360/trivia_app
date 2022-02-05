5c911f21f9655400579c97c157da1f8252720433
Kevin Diale <powersurge360@gmail.com>
2022-02-05 14:29:30 -0500

    Only support up to tablet screen size

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,5 +1,11 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
+let containerScreens = Object.assign({}, defaultTheme.screens)
+
+delete containerScreens['2xl']
+delete containerScreens['xl']
+delete containerScreens['lg']
+
 module.exports = {
   content: [
     './app/helpers/**/*.rb',
@@ -7,6 +13,9 @@ module.exports = {
     './app/views/**/*'
   ],
   theme: {
+    container: {
+      screens: containerScreens
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],

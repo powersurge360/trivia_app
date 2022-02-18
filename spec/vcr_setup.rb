abf7dcba4b8726ea5621145709706b2a02f6112d
Kevin Diale <powersurge360@gmail.com>
2022-02-17 21:13:21 -0500

    Moved where vcr lives

--- spec/vcr_setup.rb
+++ spec/vcr_setup.rb
@@ -1,7 +1,7 @@
 require "vcr"
 
 VCR.configure do |c|
-  c.cassette_library_dir = "vcr_cassettes"
+  c.cassette_library_dir = "spec/vcr_cassettes"
   c.hook_into :faraday
   c.configure_rspec_metadata!
 end

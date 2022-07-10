5c13e7c3cec2b0c6f0e3e2fdeed0477afecbff9c
Kevin Diale <powersurge360@gmail.com>
2022-07-09 21:13:40 -0400

    Improved player tests

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe Player, type: :model do
   describe "validations" do
-    describe "failing username validations" do
+    context "when failing username validations" do
       subject do
         invalid_player.tap { |p| p.valid? }
       end
@@ -24,7 +24,7 @@ RSpec.describe Player, type: :model do
       end
     end
 
-    describe "failing channel validations" do
+    context "when failing channel validations" do
       subject do
         FactoryBot.build(:player, channel: nil, game: nil).tap do |p|
           p.valid?
@@ -37,6 +37,59 @@ RSpec.describe Player, type: :model do
 
       it "should have a channel error" do
         expect(subject.errors).to include(:channel)
+      end
+    end
+
+    context "when failing join_code validations" do
+      context "when creating players without a join_code" do
+        subject do
+          FactoryBot.build(:player).tap do |p|
+            p.join_code = nil
+            p.valid?
+          end
+        end
+
+        it "should not be valid" do
+          expect(subject.valid?).to be false
+        end
+
+        it "should have a join_code error" do
+          expect(subject.errors).to include(:join_code)
+        end
+      end
+
+      context "when creating players with a bad join_code" do
+        subject do
+          FactoryBot.build(:player).tap do |p|
+            p.join_code = "#{p.join_code}-invalid-code"
+            p.valid?
+          end
+        end
+
+        it "should not be valid" do
+          expect(subject.valid?).to be false
+        end
+
+        it "should have a join_code error" do
+          expect(subject.errors).to include(:join_code)
+        end
+      end
+
+      context "when updating players" do
+        subject do
+          FactoryBot.create(:player).tap do |p|
+            p.username = "second-name"
+            p.join_code = "some random invalid code"
+            p.save
+          end
+        end
+        it "should be valid" do
+          expect(subject.valid?).to be true
+        end
+
+        it "should have no errors" do
+          expect(subject.errors).to be_empty
+        end
       end
     end
   end

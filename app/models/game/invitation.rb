7611a147aaf85ec971ed0a75c16ebe9cba0841ff
Kevin Diale <powersurge360@gmail.com>
2022-07-30 14:10:45 -0400

    We don't use this model after all

--- app/models/game/invitation.rb
+++
@@ -1,8 +0,0 @@
-class Game::Invitation < ApplicationRecord
-  include ActiveModel::Validations
-  attr_accessor :username, :channel, :join_code
-
-  validates :username, presence: true
-  validates :channel, presence: true
-  validates :join_code, presence: true
-end
